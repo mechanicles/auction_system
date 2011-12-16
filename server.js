9789a28ea5884e39d64106edab1620053cd262e4
Santosh <santosh.wadghule@gmail.com>
2011-12-17 00:01:48 +0530

    Added express framework

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-faye = require('faye');
+var faye = require('faye');
+var express = require('express')
+var app = express.createServer();
 
 var server = new faye.NodeAdapter({
   mount: '/auctions',
   timeout: 45
 });
 
-server.listen(12854);
-console.log('Server has started...');
+app.configure(function(){
+  app.set('views', __dirname + '/views') 
+  app.set('view options', {layout:false}) 
+  app.use("/public", express.static(__dirname + '/public'));
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+});
+
+app.get('/', function(req, res){
+  res.render('auctions.html');
+});
+
+
+server.attach(app);
+app.listen(12854)
